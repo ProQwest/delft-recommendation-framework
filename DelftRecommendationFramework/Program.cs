c5f78bd209ba4a8cbc94b735292f9589866e4b0b
Babak Loni <babak.loni@gmail.com>
2013-08-15 17:52:04 +0200

    Changed the default namespace to DRF

--- DelftRecommendationFramework/Program.cs
+++ DelftRecommendationFramework/Program.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DelftRecommendationFramework
+namespace DRF
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("test");
+
         }
     }
 }
